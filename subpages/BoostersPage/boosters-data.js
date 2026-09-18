0e4c71f94c4efd35b04cb626f820627ae33b10e1
Patricia (Sayouri) <sayouri@nnamesquad.top>
2026-09-18 12:57:26 +0200

    Update boosters-data.js

--- subpages/BoostersPage/boosters-data.js
+++ subpages/BoostersPage/boosters-data.js
@@ -1,11 +1,11 @@
 const boosters_data = [
-  { id: "448894048586170369", boosts: 58 }, //SAYO
+  { id: "448894048586170369", boosts: 60 }, //SAYO
   // Boosting since 5.01.2025 | +2 on the 4th of every month | 
-  { id: "756482164262043720", boosts: 51 }, //KIRKA
+  { id: "756482164262043720", boosts: 53 }, //KIRKA
   // Boosting since 16.11.2024 | +2 on the 15th of every month | 
-  // Data for Sayo and Kirka showing boosts as of 04.09.2026(sayo) and 15.09.2025(kirka), due to counting their boosts in one-month forward manner. Trusting they wont unboost before the next boost accumulation date.
-  { id: "692026308049240074", boosts: 14 },
-  // Boosting since 27.08.2026 | +2 on the 26th of every month | 
+  // Data for Sayo and Kirka showing boosts as of 04.10.2026(sayo) and 15.10.2026(kirka), due to counting their boosts in one-month forward manner. Trusting they wont unboost before the next boost accumulation date.
+  { id: "692026308049240074", boosts: 14 }, //Kotek
+  // Boosting since 27.08.2026 | +2 on the 26th of every month | (Remove if boost deleted before 26.09.2026)
   { id: "239932087925342209", boosts: 10 },
   { id: "531129228532645888", boosts: 6 },
   { id: "429661571019571200", boosts: 6 },
@@ -16,6 +16,8 @@ const boosters_data = [
   { id: "762262927477964800", boosts: 2 },
   { id: "376320713101148163", boosts: 2 },
   { id: "636210705632067586", boosts: 2 },
+  { id: "1158020020832579596", boosts: 2 }, //Felix
+  // Boosting since 17.09.2026 | +2 on the 16th of every month | (Remove if boost deleted before 16.10.2026)
   { id: "794910561891647498", boosts: 1 },
   { id: "613661919676596225", boosts: 1 },
   { id: "657650990786281486", boosts: 1 },
@@ -23,8 +25,8 @@ const boosters_data = [
   // ID 0 means Users who deleted their account or asked to remove their stats.
   { id: "0", boosts: 5 }
 ];
-// Last deleted:
-// 1142127344862122125 - 2
 
-// Last update: ID:692026308049240074 Action: Added 2 boosts.
-const boosters_updatetime = "2026-08-31T21:26:00+02:00"; // leave +02:00 for Warsaw timezone
+// Last update:
+// ID:448894048586170369 + ID:756482164262043720 Action: Added 2 boosts for date starting at 4.10 and 15.10 respectively.
+// ID:1158020020832579596 Action: Added 2 boosts.
+const boosters_updatetime = "2026-09-18T13:00:00+02:00"; // leave +02:00 for Warsaw timezone
